36154c59c1fff5177ca393c302a356759266fd00
Sébastien Menard <sebastien.menard.github@gmail.com>
2023-11-22 16:26:38 -0500

    build(benchmarks): create other type of benchmark and display benchmarks series results (#9)

--- src/web_crawler/crawler_selenium/benchmark.py
+++ src/web_crawler/crawler_selenium/benchmark.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 import logging
+from typing import TYPE_CHECKING, Any, Callable
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -12,21 +13,75 @@ from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.logger import set_logger
 
+if TYPE_CHECKING:
+    from selenium.webdriver.chrome.webdriver import WebDriver
 
-def benchmark_selenium(url: str, selector: str) -> list[str]:
-    """Benchmark Selenium.
+
+def selenium_extract_table(
+    driver: WebDriver,
+    driver_wait: WebDriverWait,
+    url: str,
+    selector: str,
+) -> list[str]:
+    """Extract table using Selenium."""
+    driver.get(url=url)
+    driver_wait.until(
+        expected_conditions.presence_of_all_elements_located(
+            (By.CSS_SELECTOR, selector),
+        ),
+    )
+    elements = driver.find_elements(by=By.CSS_SELECTOR, value=selector)
+    return [element.text for element in elements]
+
+
+def selenium_extract_lots(
+    driver: WebDriver,
+    driver_wait: WebDriverWait,
+    url: str,
+    lots: list[str],
+) -> dict[str, float]:
+    """Extract lots using Selenium."""
+    driver.get(url=url)
+    driver_wait.until(
+        expected_conditions.presence_of_element_located(
+            (By.CSS_SELECTOR, "button[mat-dialog-close]"),
+        ),
+    ).click()
+    search_input = driver.find_element(by=By.CSS_SELECTOR, value="#mat-input-0")
+
+    areas = {}
+
+    for lot in lots:
+        search_input.send_keys(lot)
+        search_input.clear()
+        driver_wait.until(
+            expected_conditions.presence_of_element_located(
+                (By.XPATH, f"//h4[contains(text(), '{lot}')]"),
+            ),
+        ).click()
+        area = driver_wait.until(
+            expected_conditions.presence_of_element_located(
+                (
+                    By.XPATH,
+                    "//td[contains(text(), 'Superficie (pi2)')]/following-sibling::td",
+                ),
+            ),
+        ).get_attribute("innerText")
+        areas[lot] = float(area.replace(" ", "").replace(",", "."))
+
+    return areas
+
+
+def use_selenium(
+    callback: Callable | None = None,
+    **kwargs: dict[str, Any],
+) -> list[str]:
+    """Use Selenium with a callback.
 
     Parameters
     ----------
-    url : str
-        The url to fetch.
-    selector : str
-        The selector to extract data from.
-
-    Returns
-    -------
-    list[str]
-        The extracted data.
+    callback : Callable
+        The callback to use with the browser context.
     """
     logger = logging.getLogger("selenium")
     logger.setLevel(level=logging.CRITICAL)
@@ -36,13 +91,6 @@ def benchmark_selenium(url: str, selector: str) -> list[str]:
     service = ChromeService(ChromeDriverManager().install())
     driver = webdriver.Chrome(options=options, service=service)
     driver_wait = WebDriverWait(driver=driver, timeout=10)
-    driver.get(url=url)
-    driver_wait.until(
-        expected_conditions.presence_of_all_elements_located(
-            (By.CSS_SELECTOR, selector),
-        ),
-    )
-    elements = driver.find_elements(by=By.CSS_SELECTOR, value=selector)
-    data = [element.text for element in elements]
+    if callback:
+        callback(driver=driver, driver_wait=driver_wait, **kwargs)
     driver.quit()
-    return data
