bef9d47733087b1a0ef6f7a1591a89ece9c3736e
Sébastien Menard <sebastien.menard.github@gmail.com>
2023-11-22 16:24:46 -0500

    build(benchmarks): create other type of benchmark and display benchmarks series results

--- src/web_crawler/crawler_playwright/benchmark.py
+++ src/web_crawler/crawler_playwright/benchmark.py
@@ -2,31 +2,63 @@
 
 from __future__ import annotations
 
-from playwright.sync_api import sync_playwright
+from typing import Any, Callable
+
+from playwright.sync_api import Page, sync_playwright
 
 
-def benchmark_playwright(url: str, selector: str) -> list[str]:
-    """Benchmark Playwright.
+def playwright_extract_table(
+    page: Page,
+    url: str,
+    selector: str,
+) -> list[str]:
+    """Extract table using Playwright."""
+    page.goto(url=url)
+    page.wait_for_selector(selector=selector)
+    elements = page.locator(selector=f"css={selector}")
+    return [element.text_content() for element in elements.element_handles()]
+
+
+def playwright_extract_lots(
+    page: Page,
+    url: str,
+    lots: list[str],
+) -> dict[str, float]:
+    """Extract lost using Playwright."""
+    page.goto(url=url)
+    page.locator(selector="button[mat-dialog-close]").click()
+    search_input = page.locator(selector="#mat-input-0")
+
+    areas = {}
+
+    for lot in lots:
+        search_input.fill("")
+        search_input.type(text=lot)
+        search_input.press(key="Enter")
+        page.locator(selector=f"//h4[contains(text(), '{lot}')]").click()
+        area = page.locator(
+            selector="//td[contains(text(), 'Superficie (pi2)')]/following-sibling::td",
+        ).text_content()
+        areas[lot] = float(area.replace(" ", "").replace(",", "."))
+
+    return areas
+
+
+def use_playwright(
+    callback: Callable | None = None,
+    **kwargs: dict[str, Any],
+) -> None:
+    """Use Playwright with a callback.
 
     Parameters
     ----------
-    url : str
-        The url to fetch.
-    selector : str
-        The selector to extract data from.
-
-    Returns
-    -------
-    list[str]
-        The extracted data.
+    callback : Callable
+        The callback to use with the browser context.
     """
     with sync_playwright() as pw:
         browser = pw.chromium.launch()
         context = browser.new_context()
         page = context.new_page()
-        page.goto(url=url)
-        page.wait_for_selector(selector=selector)
-        elements = page.locator(selector=f"css={selector}")
-        data = [element.text_content() for element in elements.element_handles()]
+        if callback:
+            callback(page=page, **kwargs)
         browser.close()
-    return data
