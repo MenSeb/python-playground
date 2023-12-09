ff27e8b3553f5f5de8a735a694f1d9d782fe21b6
Sébastien Menard <sebastien.menard.github@gmail.com>
2023-12-08 21:36:42 -0500

    build(tracing): test web spider from server and client

--- src/html_tracing/server.py
+++ src/html_tracing/server.py
@@ -3,13 +3,20 @@
 from __future__ import annotations
 
 import contextlib
-from os import environ
 
-from dotenv import find_dotenv, load_dotenv
-from flask import Flask, jsonify, render_template
-from utils import fetch_urls
+from flask import Flask, jsonify, render_template, request
+from web_scraping.agents import UserAgents
+from web_scraping.proxies import Proxies, Query
+from web_scraping.session import Session
+from web_spider.utilities import extract_domain, fetch_links
 
-load_dotenv(find_dotenv())
+agents = UserAgents()
+proxies = Proxies()
+session = Session()
+
+agents.refresh(refresh_time=60 * 60)
+proxies.refresh(refresh_time=60 * 60)
+
 
 app = Flask(
     import_name=__name__,
@@ -24,16 +31,58 @@ def route_home() -> str:
     return render_template("index.html")
 
 
-@app.route("/api/tracing/<url>")
-def api_tracing(url: str) -> str:
+@app.route("/api/spider", methods=["POST"])
+def api_spider() -> str:
     """Trace a URL."""
-    return jsonify(urls=fetch_urls(url=url))
+    form = request.form
+    url = form.get("url")
+
+    list_agents = agents.extract(limit=10)
+    list_proxies = proxies.extract(
+        limit=10,
+        dataframe=proxies.query(
+            queries=[
+                Query(
+                    data="US",
+                    key=proxies.headers.code,
+                    operator=proxies.operators.eq,
+                ),
+                Query(
+                    data="elite proxy",
+                    key=proxies.headers.anonymity,
+                    operator=proxies.operators.eq,
+                ),
+            ],
+            dataframe=proxies.load(),
+        ),
+    )
+
+    domain = extract_domain(url)
+    domain_urls = fetch_links(
+        agents=list_agents,
+        proxies=list_proxies,
+        session=session,
+        url=url,
+    )
+
+    if domain_urls is None:
+        return jsonify("Could not fetch the url...")
+
+    domains = {}
+
+    for domain_url in domain_urls:
+        urls = fetch_links(
+            agents=list_agents,
+            proxies=list_proxies,
+            session=session,
+            url=url,
+        )
+        urls_domain = extract_domain(url=domain_url)
+        domains[urls_domain] = urls
+
+    return jsonify({"domain": domain, "domains": domains})
 
 
 if __name__ == "__main__":
-    host = environ.get("HOST")
-    port = int(environ.get("PORT"))
-    debug = bool(environ.get("DEBUG"))
-
     with contextlib.suppress(KeyboardInterrupt):
-        app.run(host=host, port=port, debug=debug)
+        app.run(host="127.0.0.1", port="8080", debug=True)
--- src/html_tracing/statics/scripts/index.js
+++ src/html_tracing/statics/scripts/index.js
@@ -1,5 +1,23 @@
-function test() {
-    console.log("TEST");
+class WebSpider {
+    constructor() {
+        this.form = document.getElementById('form-spider');
+        this.form.addEventListener('submit', this.submit)
+    }
+
+    submit = async (event) => {
+        event.preventDefault()
+
+        const formData = new FormData(this.form);
+
+        const response = await fetch('/api/spider', {
+            method: "POST",
+            body: formData
+        })
+
+        const json = await response.json()
+
+        console.log({json});
+    }
 }
 
-test()
+new WebSpider()
